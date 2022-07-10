520f3055fd0e07e0a2dc4dd57c98f1de90af85ce
vkuznetsoff <ss_slavich@mail.ru>
2022-07-10 16:37:41 +0300

    Fix function to get Country code, currency code and strcode

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import "./styles.css";
 
 const apiKey = "d18ezMeDAF4oR8sVTiOZzWZIMfVAt2SadfJmDyxn";
 const geoApiKey = "16c29522b5e1d3434c4018b75148e61c";
+const daDataApiKey = "7f8a786da68ade6636ea30508ea8f44710ea1a92";
 
 let currencyList;
 
@@ -9,6 +10,12 @@ const currencyExchage = {
   from: "",
   to: "",
   course: "65",
+};
+
+let initCountry = {
+  code: "US",
+  currencySymbols: "USD",
+  currencyCode: "",
 };
 
 const selects = document.querySelectorAll("select");
@@ -19,7 +26,7 @@ const toCurrencySelect = selects[1];
 const fromInput = document.getElementById("from_input");
 const toInput = document.getElementById("to_input");
 
-toInput.disabled = true
+toInput.disabled = true;
 
 function getCurrencyCode(sourseSelect) {
   return sourseSelect.options[sourseSelect.selectedIndex].text;
@@ -31,12 +38,15 @@ window.addEventListener("load", () => {
   currencyList = ["RUB", "USD", "EUR"];
   setSelect(currencyList);
   fromInput.value = "1.00";
-
-  calcResult()
+  calcResult();
 });
 
 function calcResult() {
-    toInput.value = exchange(fromInput.value, currencyExchage.to, currencyExchage.course)
+  toInput.value = exchange(
+    fromInput.value,
+    currencyExchage.to,
+    currencyExchage.course
+  );
 }
 
 fromCurrencySelect.addEventListener("change", () => {
@@ -48,10 +58,9 @@ toCurrencySelect.addEventListener("change", () => {
   // toCurrencySelect.options[toCurrencySelect.selectedIndex].text
 });
 
-fromInput.addEventListener('input', () => {
-    calcResult()
-}
-)
+fromInput.addEventListener("input", () => {
+  calcResult();
+});
 
 function setSelect(values) {
   selects.forEach((select) => {
@@ -74,19 +83,79 @@ function getCurrencyList() {
     .then((response) => response.data);
 }
 
+function getCurrencyByCode(currCode) {
+  const url =
+    "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/currency";
+  const options = setQueryParams(currCode);
+  return fetch(url, options)
+    .then((response) => response.json())
+    .then((result) => {
+      if (result.suggestions.length !== 0)
+        initCountry.currencySymbols = result.suggestions[0].data.strcode;
+      console.log(result);
+    })
+    .catch((error) => console.log("error", error));
+}
+
+//Ищем код валюты по аббревиатуре страны
+function findCurrencyCodeByCountryStr(query) {
+  // const url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/country"
+  // const options = setQueryParams(setQueryParams(initCountry.code))
+  // fetch(url, options)
+  //   .then((response) => response.text())
+  //   .then((result) =>  console.log("result", result))
+  //    .catch((error) => console.log("error", error));
+
+  const url =
+    "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/country";
+  const token = "7f8a786da68ade6636ea30508ea8f44710ea1a92";
+  // const query = "US";
+
+  const options = {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: "Token " + token,
+    },
+    body: JSON.stringify({ query: query }),
+  };
+
+  return fetch(url, options)
+    .then((response) => response.json())
+    .then(
+      (result) => (initCountry.currencyCode = result.suggestions[0].data.code)
+    )
+    .catch((error) => console.log("error", error));
+}
+
 function getGeolocation() {
   navigator.geolocation.getCurrentPosition((position) => {
     const { latitude, longitude } = position.coords;
     getCountry(latitude, longitude);
   });
+
+  //   findCurrencyCodeByCountryStr(initCountry.code)
+  // .then( () => getCurrencyByCode(initCountry.currencyCode))
+  // .then( () => console.log(initCountry))
 }
 
 function getCountry(lat, lon) {
-  return fetch(
+  fetch(
     `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${geoApiKey}`
   )
     .then((response) => response.json())
-    .then((data) => console.log(data[0].country));
+    .then((data) => {
+      initCountry.code = data[0].country;
+      return data[0].country;
+    })
+    .then((countryCode) => findCurrencyCodeByCountryStr(countryCode)).
+    then(() => getCurrencyByCode(initCountry.currencyCode))
+    .then(() => console.log(initCountry))
+    .catch((error) => {
+      console.log("ERROR - getCountry:", error);
+    });
 }
 
 function getCurrentCourse(base, currencies) {
@@ -101,7 +170,7 @@ function getCurrentCourse(base, currencies) {
   )
     .then((response) => response.json())
     .then(
-      (response) => currencyExchage.course = response.data[currencies].value
+      (response) => (currencyExchage.course = response.data[currencies].value)
     );
 }
 
@@ -109,6 +178,24 @@ function exchange(value, base, course) {
   return (value * course).toFixed(2);
 }
 
-// getGeolocation()
+// getGeolocation();
 // getCurrencyList()
 // getCurrentCourse('USD', 'USD')
+
+function setQueryParams(query) {
+  const token = "7f8a786da68ade6636ea30508ea8f44710ea1a92";
+
+  const options = {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: "Token " + token,
+    },
+    body: JSON.stringify({ query: query }),
+  };
+  return options;
+}
+
+getGeolocation();
